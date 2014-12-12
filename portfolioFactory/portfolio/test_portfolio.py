d7ac62a3b0b1714711e76f53766bb21e4809e9f9
im965 <israelmalkin@nyu.edu>
2014-12-11 20:33:30 -0500

    added comment to unit tests

--- portfolioFactory/portfolio/test_portfolio.py
+++ portfolioFactory/portfolio/test_portfolio.py
@@ -44,14 +44,14 @@ class TestPortfolioFunctions(unittest.TestCase):
 ##############################################################################
         
     def testBadInput_NotList(self):
-        
+        # input must be in list format
         a = 55
         b = 77
         self.assertRaises(customExceptions.notListError,self.createPortfolio,a,b)
         
         
     def testBadInput_LengthMatch(self):
-        
+        # length of inputs must match
         a = [55]
         b = [77,'aa']
         self.assertRaises(customExceptions.listMismatchError,self.createPortfolio,a,b)
@@ -59,14 +59,14 @@ class TestPortfolioFunctions(unittest.TestCase):
         
         
     def testBadInput_TypeIssue(self):
-        
+        # the types in strategyPool must all be strategy objects
         a = [55,55]
         b = [77,'aa']
         self.assertRaises(customExceptions.notStrategyError,self.createPortfolio,a,b)
         
         
-    def testBadInput_TypeIssue(self):
-        
+    def testBadInput_BadWeight(self):
+        # weights must numeric
         configUni = 'uni_early'
         configStr = '1_5'
         testUni = self.createUniverse(configUni)
--- portfolioFactory/strategy/test_strategy.py
+++ portfolioFactory/strategy/test_strategy.py
@@ -39,7 +39,7 @@ class TestStrategyFunctions(unittest.TestCase):
 ##############################################################################
         
     def testBadConfig_ConfigPath(self):
-        
+        # bad path for config file
         configUni = 'uni_1_5'
         configStr = 'made_up'
         testUni = self.createUniverse(configUni)
@@ -47,42 +47,42 @@ class TestStrategyFunctions(unittest.TestCase):
         
         
     def testBadConfig_SignalPath(self):
-        
+        # bad path for signal file path
         configUni = 'uni_1_5'
         configStr = 'bad_signal'
         testUni = self.createUniverse(configUni)
         self.assertRaises(customExceptions.invalidSignalPath,self.createStrategy,testUni,configStr)
 
     def testBadConfig_MissingInput(self):
-        
+        # config file missing an input
         configUni = 'uni_1_5'
         configStr = 'missing_input'
         testUni = self.createUniverse(configUni)
         self.assertRaises(customExceptions.missingInput,self.createStrategy,testUni,configStr)
         
     def testBadConfig_UnexpectedInput(self):
-        
+        # config file encounters an unexpected input
         configUni = 'uni_1_5'
         configStr = 'extra_spec'
         testUni = self.createUniverse(configUni)
         self.assertRaises(customExceptions.unexpectedInput,self.createStrategy,testUni,configStr)
         
     def testBadConfig_StringRule(self):
-        
+        # rule parameter must be numeric
         configUni = 'uni_1_5'
         configStr = 'string_rule'
         testUni = self.createUniverse(configUni)
         self.assertRaises(customExceptions.ruleNotInt,self.createStrategy,testUni,configStr)
         
     def testBadConfig_WindowInt(self):
-        
+        # window paramter must be integer
         configUni = 'uni_1_5'
         configStr = 'string_rebalance'
         testUni = self.createUniverse(configUni)
         self.assertRaises(customExceptions.windowNotInt,self.createStrategy,testUni,configStr)
         
     def testBadConfig_WindowNeg(self):
-        
+        # window parameter must be positive
         configUni = 'uni_1_5'
         configStr = 'negative_rebalance'
         testUni = self.createUniverse(configUni)
@@ -94,14 +94,14 @@ class TestStrategyFunctions(unittest.TestCase):
 ##############################################################################
         
     def testOverlap_Tickers(self):
-        
+        # no overlap between signal ticker and return tickers
         configUni = 'uni_1_2'
         configStr = '4_5'
         testUni = self.createUniverse(configUni)
         self.assertRaises(customExceptions.noTickerOverlap,self.createStrategy,testUni,configStr)
         
     def testOverlap_dates(self):
-        
+        # no overlap in the date spans of returns and signal
         configUni = 'uni_early'
         configStr = '1_2_late'
         testUni = self.createUniverse(configUni)
@@ -112,7 +112,7 @@ class TestStrategyFunctions(unittest.TestCase):
 ##############################################################################
 
     def testNoSignal(self):
-        
+        # signal data is missing, not enough obs to make selection
         configUni = 'uni_early'
         configStr = 'no_signal'
         testUni = self.createUniverse(configUni)
