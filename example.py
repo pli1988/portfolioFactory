e0b321b61b280dd33f4153c3b4d1f86ac52c09e4
Peter Li <p.li1988@gmail.com>
2014-12-11 21:25:12 -0500

    modify example

--- example.py
+++ example.py
@@ -22,7 +22,7 @@ from portfolioFactory.utils import getFileLocation
 #usEqUniverse = universe.universe('portfolioFactory/universe/usEquityConfig.txt')
 
 
-assetReturnsPath = getFileLocation.getFileLocation()
+#assetReturnsPath = getFileLocation.getFileLocation()
 
 
 #configFilePath = getFileLocation.getFileLocation()
@@ -34,17 +34,62 @@ configFilePath = '/home/peter/Documents/Homework/ProgrammingForDatascience/final
 
 usEqUniverse = universe.universe('testUniverse', assetReturnsPath)
 testStrategy = strategy.strategy(usEqUniverse, configFilePath)
-testPortfolio =  portfolio.portfolio([testStrategy],[1])
+testPortfolio =  portfolio.portfolio([testStrategy],[1]) 
 
-    
-    
-
-
+###############################################################################
+# Plotting
+###############################################################################
 
 testSeries = testStrategy.strategyReturns
 
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+np.random.seed(sum(map(ord, "aesthetics")))
+
+import seaborn as sns
+
+txt = '''
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.'''
+    
+    
+f, axarr = plt.subplots(3, sharex = True)
+
+axarr[0].plot(testSeries.index,retMetrics.rollingReturn(testSeries,12), 'r')
+axarr[1].plot(testSeries.index,retMetrics.rollingReturn(testSeries,36), 'g')
+axarr[2].plot(testSeries.index,retMetrics.rollingReturn(testSeries,60))
+
+#
+#f.text(.1,.1,txt)
+f.show()
+
+fig, axes = plt.subplots(3, sharex = True)
+
+retMetrics.rollingReturn(testSeries,12).plot(ax = axes[0])
+retMetrics.rollingReturn(testSeries,36).plot(ax = axes[1])
+retMetrics.rollingReturn(testSeries,50).plot(ax = axes[2])
+
+axes[0].set_xlabel('')
+axes[1].set_xlabel('')
+
+axes[0].set_title('Rolling 1-Year Return')
+axes[1].set_title('Rolling 3-Year Return')
+axes[2].set_title('Rolling 5-Year Return')
+
+fig.show()
+
+
+# Metrics
 utils.checkSeqentialMonthly(testSeries.index)
 riskMetrics.maxDrawdown(testSeries)
+
 riskMetrics.VaR(testSeries, 12, 95)
 retMetrics.rollingReturn(testSeries, 6).plot()
 
