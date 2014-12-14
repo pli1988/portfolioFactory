c9b926bf4d797863936887f5b0487dff4abd058c
im965 <israelmalkin@nyu.edu>
2014-12-13 22:44:44 -0500

    added print statements to strategy

--- example01_Greg.py
+++ example01_Greg.py
@@ -6,6 +6,7 @@ Author: Israel Malkin
 
 import pandas as pd
 import portfolioFactory
+import warnings
 import portfolioFactory.universe.universe as universe
 import portfolioFactory.strategy.strategy as strategy
 import portfolioFactory.portfolio.portfolio as portfolio
@@ -17,8 +18,10 @@ location = './ExampleFiles/'
 
 
 universe_Ex1 = universe.universe('Universe_Ex1',location+'totalReturnData')
-rolling_returns_12m = calcRollingReturns(universe_Ex1.assetReturns,12).shift(period=1).truncate(before="01/01/1995",after="31/01/2012")
-rolling_volatility_24m = pd.rolling_std(universe_Ex1.assetReturns,24).shift(period=1).truncate(before="01/01/1995",after="31/01/2012")
+rolling_returns_12m = calcRollingReturns(universe_Ex1.assetReturns,12).shift(period=1).truncate(before="01/01/1991")
+rolling_volatility_12m = pd.rolling_std(universe_Ex1.assetReturns,12).shift(period=1).truncate(before="01/01/1991")
 
-rolling_returns_12m.to_pickle(location+'rolling_returns_12m')
-rolling_volatility_24m.to_pickle(location+'rolling_volatility_24m')
+    
+
+    
+
